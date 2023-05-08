98f256e601e359fbd11bdd337dcfcc42f8591823
raquel <m-raquel@outlook.com.br>
2023-05-08 00:50:28 -0300

    component sidebar

--- ignite-feed/src/App.tsx
+++ ignite-feed/src/App.tsx
@@ -1,11 +1,20 @@
 import { Header } from "./Components/Header"
+import { Sidebar } from "./Components/Sidebar"
+
+import styles from "./styles/app.module.scss"
 
 function App() {
 
   return (
     <>
       <Header />
-      <h1>hello</h1>
+      <div className={styles.wrapper}>
+        <Sidebar />
+        <main>
+          <h1>post</h1>
+          <h1>post</h1>
+        </main>
+      </div>
     </>
   )
 }
---
+++ ignite-feed/src/Components/Sidebar/index.tsx
@@ -0,0 +1,30 @@
+import styles from "./styles.module.scss"
+
+import { PencilSimpleLine } from "@phosphor-icons/react"
+
+export function Sidebar(){
+    return (
+        <aside className={styles.sidebar}>
+            <img 
+                src="https://images.unsplash.com/photo-1567863786964-9d65fa4469ed?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=500&q=80" 
+                alt="imagem de cover escolhida pelo usuário" 
+                className={styles.cover}
+            />
+            <div className={styles.profile}>
+                <img 
+                    src="https://github.com/M-RaquelCS.png" 
+                    alt=""
+                />
+
+                <strong>Maria Raquel</strong>
+                <span>Software Engineer</span>
+            </div>
+            <footer>
+                <a href="#">
+                    <PencilSimpleLine size={20} />
+                    Editar seu perfil
+                </a>
+            </footer>
+        </aside>
+    )
+}
