9990be0812ba935e632c0fa7c4eb7599fd128e30
raquel <m-raquel@outlook.com.br>
2023-05-08 13:35:26 -0300

    formulário de comentários

--- ignite-feed/src/Components/Post/index.tsx
+++ ignite-feed/src/Components/Post/index.tsx
@@ -35,6 +35,16 @@ export function Post(){
                     <a href="#">#rocketseat</a>
                 </p>
             </div>
+
+            <form className={styles.commentForm}>
+                <strong>Deixe seu feedback</strong>
+
+                <textarea placeholder="Deixe seu feedback" />
+
+                <footer>
+                    <button type="submit">Publicar</button>
+                </footer>
+            </form>
         </article>
     )
 }
