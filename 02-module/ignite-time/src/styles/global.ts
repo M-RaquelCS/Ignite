913dd92577fbfe9b5bf0282d5521c406e966fe61
raquel <m-raquel@outlook.com.br>
2023-06-04 18:10:06 -0300

    configurando o ESLINT padrão rocketseat e configurando o router-dom

--- 02-module/ignite-time/src/styles/global.ts
+++ 02-module/ignite-time/src/styles/global.ts
@@ -9,7 +9,7 @@ export const GlobalStyle = createGlobalStyle`
 
   :focus {
     outline: 0;
-    box-shadow: 0 0 0 2px ${props => props.theme['green-500']};
+    box-shadow: 0 0 0 2px ${(props) => props.theme['green-500']};
   }
 
   body, input, textarea, button {
@@ -19,7 +19,7 @@ export const GlobalStyle = createGlobalStyle`
   }
 
   body {
-    background: ${props => props.theme['gray-900']};
-    color: ${props => props.theme['gray-300']};
+    background: ${(props) => props.theme['gray-900']};
+    color: ${(props) => props.theme['gray-300']};
   }
-`;
+`
