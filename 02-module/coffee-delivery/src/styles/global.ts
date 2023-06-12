a092839f99aea6c24dc1e107f5e6fee4cd5afe94
raquel <m-raquel@outlook.com.br>
2023-06-12 12:10:20 -0300

    configurações iniciais, de themes, fonts

---
+++ 02-module/coffee-delivery/src/styles/global.ts
@@ -0,0 +1,23 @@
+import { createGlobalStyle } from 'styled-components'
+
+export const GlobalStyle = createGlobalStyle`
+  *{
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
+
+  :focus {
+    outline: 0;
+    box-shadow: 0 0 0 2px ${(props) => props.theme['yellow-700']}
+  }
+
+  body, input, textarea, button {
+    font-family: 'Roboto', sans-serif;
+    font-size: 1rem;
+  }
+
+  h1,h2,h3,h4,h5,h6 {
+    font-family: 'Baloo 2', sans-serif;
+  }
+`
