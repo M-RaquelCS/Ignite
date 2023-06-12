1e5d2e587176019ced82942ebc86c6d1efefef18
raquel <m-raquel@outlook.com.br>
2023-06-12 15:44:09 -0300

    header component

--- 02-module/coffee-delivery/src/Components/Header/index.tsx
+++ 02-module/coffee-delivery/src/Components/Header/index.tsx
@@ -1,18 +1,26 @@
+import { MapPin, ShoppingCart } from 'phosphor-react'
 import logoImg from '../../assets/logo.png'
 import { HeaderContainer } from './styles'
+
+import { NavLink } from 'react-router-dom'
 
 export function Header() {
   return (
     <HeaderContainer>
-      <img
-        src={logoImg}
-        alt="a logo consiste em um copo de café com tampa na cor roxa e com um desenho em branco de um foguete de decolando com um caminho de fumaça atrás e ao lado o nome da cafeteria 'Coffee Delivery' com uma palavra abaixo da outra"
-      />
+      <NavLink to="/">
+        <img
+          src={logoImg}
+          alt="a logo consiste em um copo de café com tampa na cor roxa e com um desenho em branco de um foguete de decolando com um caminho de fumaça atrás e ao lado o nome da cafeteria 'Coffee Delivery' com uma palavra abaixo da outra"
+        />
+      </NavLink>
       <nav>
-        <div>
-          <p>Belo Jardim, PE</p>
-        </div>
-        <a href="#">store</a>
+        <NavLink to="#" title="Google Maps">
+          <MapPin size={22} weight="fill" />
+          Belo Jardim, PE
+        </NavLink>
+        <NavLink to="/checkout" title="Carrinho">
+          <ShoppingCart size={22} weight="fill" />
+        </NavLink>
       </nav>
     </HeaderContainer>
   )
