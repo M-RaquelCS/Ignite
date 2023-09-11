9742ef2ff45bcc0ab0858acbac991bf0ec907fd4
maria raquel <m-raquel@outlook.com.br>
2023-09-11 10:46:19 -0300

    formatação dos valores e datas

--- 03-module/dt-money/src/components/Summary/index.tsx
+++ 03-module/dt-money/src/components/Summary/index.tsx
@@ -1,7 +1,26 @@
+import { useContext } from "react";
+import { TransactionsContext } from "../../contexts/TransactionsContext";
+
 import { ArrowCircleDown, ArrowCircleUp, CurrencyDollar } from "phosphor-react";
 import { Card, Container } from "./styles";
+import { priceFormatter } from "../../utils/formatter";
 
 export function Summary(){
+  const { transactions } = useContext(TransactionsContext)
+
+  // reduce -> reduzir o array em uma nova estrtura -> {income: 0 , outcome: 0, total: 0}
+  const summary = transactions.reduce((acumulator, transaction) => {
+    if (transaction.type === 'income'){
+      acumulator.income += transaction.price
+      acumulator.total += transaction.price
+    } else {
+      acumulator.outcome += transaction.price
+      acumulator.total -= transaction.price
+    }
+    
+    return acumulator;
+  }, {income: 0 , outcome: 0, total: 0})
+  // segundo parametro -> estrutura -> inicial
   return (
     <Container>
       <Card>
@@ -9,21 +28,21 @@ export function Summary(){
           <span>Entradas</span>
           <ArrowCircleUp size={32} color="#00b37e" />
         </header>
-        <strong>R$ 17.400,00</strong>
+        <strong>{priceFormatter.format(summary.income)}</strong>
       </Card>
       <Card>
         <header>
           <span>Saídas</span>
           <ArrowCircleDown size={32} color="#f75a68" />
         </header>
-        <strong>R$ 17.400,00</strong>
+        <strong>{priceFormatter.format(summary.outcome)}</strong>
       </Card>
       <Card variant="green">
         <header>
           <span>Total</span>
           <CurrencyDollar size={32} color="#fff" />
         </header>
-        <strong>R$ 17.400,00</strong>
+        <strong>{priceFormatter.format(summary.total)}</strong>
       </Card>
     </Container>
   )
