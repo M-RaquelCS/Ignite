3d1897406856b9cbfa21488ed32964448fe58d70
Raquel Santos <m-raquel@outlook.com.br>
2024-07-07 18:05:40 -0300

    use compoment form with react-hook-forms and zod

--- pizza.shop-web/src/pages/auth/sign-in.tsx
+++ pizza.shop-web/src/pages/auth/sign-in.tsx
@@ -1,10 +1,36 @@
-import { Label } from '@radix-ui/react-label'
+import { zodResolver } from '@hookform/resolvers/zod'
 import { Helmet } from 'react-helmet-async'
+import { useForm } from 'react-hook-form'
+import { z } from 'zod'
 
 import { Button } from '../../components/ui/button'
+import {
+  Form,
+  FormControl,
+  // FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from '../../components/ui/form'
 import { Input } from '../../components/ui/input'
 
+// form with shadcn/ui form doc
+const schema = z.object({
+  email: z
+    .string({ message: 'Por favor insira seu e-mail cadastrado.' })
+    .email({ message: 'Por favor insira um e-mail válido.' }),
+})
+
 export function SignIn() {
+  const form = useForm<z.infer<typeof schema>>({
+    resolver: zodResolver(schema),
+  })
+
+  function onSubmit(values: z.infer<typeof schema>) {
+    console.log(values)
+  }
+
   return (
     <>
       <Helmet title="Login" />
@@ -19,16 +45,33 @@ export function SignIn() {
             </p>
           </div>
 
-          <form className="space-y-4">
-            <div className="space-y-2">
-              <Label htmlFor="email">Seu e-mail</Label>
-              <Input id="email" />
-            </div>
-
-            <Button className="w-full" type="submit">
-              Acessar painel
-            </Button>
-          </form>
+          <Form {...form}>
+            <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-4">
+              <FormField
+                control={form.control}
+                name="email"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Seu e-mail</FormLabel>
+                    <FormControl>
+                      <Input
+                        id="email"
+                        placeholder="example@example.com"
+                        {...field}
+                      />
+                    </FormControl>
+                    {/* <FormDescription>
+                      Por favor entre com um e-mail cadastrado
+                    </FormDescription> */}
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+              <Button className="w-full" type="submit">
+                Acessar painel
+              </Button>
+            </form>
+          </Form>
         </div>
       </div>
     </>
