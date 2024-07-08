bd86b073e0b77331f8219b665b7ec646622e0cf3
Raquel Santos <m-raquel@outlook.com.br>
2024-07-07 23:18:38 -0300

    create and styled page create account

--- pizza.shop-web/src/pages/auth/sign-in.tsx
+++ pizza.shop-web/src/pages/auth/sign-in.tsx
@@ -1,6 +1,7 @@
 import { zodResolver } from '@hookform/resolvers/zod'
 import { Helmet } from 'react-helmet-async'
 import { useForm } from 'react-hook-form'
+import { Link } from 'react-router-dom'
 import { toast } from 'sonner'
 import { z } from 'zod'
 
@@ -8,6 +9,7 @@ import { Button } from '../../components/ui/button'
 import {
   Form,
   FormControl,
+  FormDescription,
   // FormDescription,
   FormField,
   FormItem,
@@ -53,6 +55,13 @@ export function SignIn() {
     <>
       <Helmet title="Login" />
       <div className="p-8">
+        <Button
+          variant={'secondary'}
+          asChild
+          className="absolute right-8 top-8"
+        >
+          <Link to={'/sign-up'}>Ir para cadastro</Link>
+        </Button>
         <div className="flex w-[320px] flex-col justify-center gap-6">
           <div className="flex flex-col gap-2 text-center">
             <h1 className="text-2xl font-semibold tracking-tight">
@@ -78,9 +87,9 @@ export function SignIn() {
                         {...field}
                       />
                     </FormControl>
-                    {/* <FormDescription>
+                    <FormDescription className="text-xs">
                       Por favor entre com um e-mail cadastrado
-                    </FormDescription> */}
+                    </FormDescription>
                     <FormMessage />
                   </FormItem>
                 )}
