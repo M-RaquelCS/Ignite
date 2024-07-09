30015175e5b4b9e2baee9443445b5a40a37ddbb6
Raquel Santos <m-raquel@outlook.com.br>
2024-07-08 21:43:31 -0300

    create component for details ticket

--- pizza.shop-web/src/pages/app/orders/order-table-row.tsx
+++ pizza.shop-web/src/pages/app/orders/order-table-row.tsx
@@ -1,17 +1,24 @@
 import { ArrowRight, Search, X } from 'lucide-react'
 
 import { Button } from '../../../components/ui/button'
+import { Dialog, DialogTrigger } from '../../../components/ui/dialog'
 import { TableCell, TableRow } from '../../../components/ui/table'
+import { OrderDetails } from './order-details'
 
 export function OrderTableRow() {
   return (
     <TableRow>
       <TableCell>
-        <Button variant={'outline'} size={'icon'}>
-          <Search className="h-4 w-4" />
-          {/* Visivel apenas para o leitor de tela */}
-          <span className="sr-only">Detalhes do pedido</span>
-        </Button>
+        <Dialog>
+          <DialogTrigger asChild>
+            <Button variant={'outline'} size={'icon'}>
+              <Search className="h-4 w-4" />
+              {/* Visivel apenas para o leitor de tela */}
+              <span className="sr-only">Detalhes do pedido</span>
+            </Button>
+          </DialogTrigger>
+          <OrderDetails />
+        </Dialog>
       </TableCell>
       <TableCell className="font-mono text-xs font-medium">
         ad3123ewqsa
