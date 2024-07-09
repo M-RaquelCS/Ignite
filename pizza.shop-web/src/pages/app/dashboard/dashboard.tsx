7f6c0ac96ba2abec2f446df9efbec5b9f6e5bd88
Raquel Santos <m-raquel@outlook.com.br>
2024-07-08 22:43:39 -0300

    create component for details on dashboard

---
+++ pizza.shop-web/src/pages/app/dashboard/dashboard.tsx
@@ -0,0 +1,24 @@
+import { Helmet } from 'react-helmet-async'
+
+import { DayOrdersAmountCard } from './day-month-amount-card'
+import { MonthOrdersAmount } from './month-orders-amount-card'
+import { MonthOrdersCanceledAmount } from './month-orders-canceled-amount-card'
+import { MonthRevenueCard } from './month-revenue-card'
+
+export function Dashboard() {
+  return (
+    <>
+      <Helmet title="Dashboard" />
+      <div className="flex flex-col gap-4">
+        <h1 className="text-3xl font-bold tracking-tight">Dashboard</h1>
+
+        <div className="grid grid-cols-4 gap-4">
+          <MonthRevenueCard />
+          <MonthOrdersAmount />
+          <DayOrdersAmountCard />
+          <MonthOrdersCanceledAmount />
+        </div>
+      </div>
+    </>
+  )
+}
