a0b8d9e235ebea15d87271b69b98a6903ddb7097
Raquel Santos <m-raquel@outlook.com.br>
2024-07-13 14:38:41 -0300

    create status components

--- pizza.shop/pizza.shop-web/src/pages/app/orders/order-table-row.tsx
+++ pizza.shop/pizza.shop-web/src/pages/app/orders/order-table-row.tsx
@@ -1,14 +1,18 @@
+import { formatDistanceToNow } from 'date-fns'
+import { ptBR } from 'date-fns/locale'
+
 import { ArrowRight, Search, X } from 'lucide-react'
 
 import { Button } from '../../../components/ui/button'
 import { Dialog, DialogTrigger } from '../../../components/ui/dialog'
 import { TableCell, TableRow } from '../../../components/ui/table'
 import { OrderDetails } from './order-details'
+import { OrderStatus } from '../../../components/status'
 
 interface OrderTableRowProps {
   order: {
     orderId: string;
-    createdAt: Date
+    createdAt: string
     status: "pending" | "canceled" | "processing" | "delivering" | "delivered";
     customerName: string;
     total: number;
@@ -33,15 +37,21 @@ export function OrderTableRow({ order }: OrderTableRowProps) {
       <TableCell className="font-mono text-xs font-medium">
         {order.orderId}
       </TableCell>
-      <TableCell className="text-muted-foreground">há 15 min</TableCell>
+      <TableCell className="text-muted-foreground">
+        {formatDistanceToNow(order.createdAt, {
+          locale: ptBR,
+          addSuffix: true,
+        })}
+      </TableCell>
       <TableCell>
-        <div className="flex items-center gap-2">
-          <span className="h-2 w-2 rounded-full bg-slate-400" />
-          <span className="font-medium text-muted-foreground">{order.status}</span>
-        </div>
+        <OrderStatus status={order.status} />
       </TableCell>
       <TableCell className="font-medium">{order.customerName}</TableCell>
-      <TableCell className="font-medium">{order.total}</TableCell>
+      <TableCell className="font-medium">{order.total.toLocaleString('pt-BR', {
+          style: 'currency',
+          currency: 'BRL',
+        })}
+      </TableCell>
       <TableCell>
         <Button variant={'outline'} size={'xs'}>
           <ArrowRight className="mr-2 h-4 w-4" />
