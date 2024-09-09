84b592f725c044d6ad984bc8945f719c32201757
Raquel Santos <m-raquel@outlook.com.br>
2024-09-09 01:19:04 -0300

    add loading skeleton in orders table

--- pizza.shop/pizza.shop-web/src/pages/app/orders/orders.tsx
+++ pizza.shop/pizza.shop-web/src/pages/app/orders/orders.tsx
@@ -14,6 +14,7 @@ import { useQuery } from '@tanstack/react-query'
 import { getOrders } from '../../../api/orders/get-orders'
 import { useSearchParams } from 'react-router-dom'
 import { z } from 'zod'
+import { OrderTableSkeleton } from './loading/order-table-skeleton'
 
 export function Orders() {
 
@@ -27,7 +28,7 @@ export function Orders() {
     .transform(page => page -1)
     .parse(searchParams.get('page') ?? '1')
 
-  const { data: result } = useQuery({
+  const { data: result, isLoading: isLoadingOrders } = useQuery({
     queryKey: ['orders', pageIndex, orderId, customerName, status],
     queryFn: () => getOrders({ 
       pageIndex, 
@@ -69,6 +70,7 @@ export function Orders() {
                 </TableRow>
               </TableHeader>
               <TableBody>
+                {isLoadingOrders && <OrderTableSkeleton />}
                 {result && result.data.orders.map(order => {
                   return <OrderTableRow key={order.orderId} order={order} />
                 }) }
