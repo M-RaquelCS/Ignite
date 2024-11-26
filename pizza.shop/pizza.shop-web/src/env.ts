d89556706ee139979d6f0d854288667959a6cc1f
Raquel Santos <m-raquel@outlook.com.br>
2024-11-25 21:20:12 -0300

    setup msw

--- pizza.shop/pizza.shop-web/src/env.ts
+++ pizza.shop/pizza.shop-web/src/env.ts
@@ -2,6 +2,7 @@ import { z } from 'zod'
 
 export const envSchema = z.object({
   VITE_API_URL: z.string().url(),
+  MODE: z.enum(['production', 'development', 'test']),
 })
 
 export const env = envSchema.parse(import.meta.env)
--- pizza.shop/pizza.shop-web/src/main.tsx
+++ pizza.shop/pizza.shop-web/src/main.tsx
@@ -4,9 +4,12 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 
 import { App } from './app.tsx'
+import { enableWorker } from './api/mocks/index.ts'
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+enableWorker().then(() => {
+  ReactDOM.createRoot(document.getElementById('root')!).render(
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  )
+})
