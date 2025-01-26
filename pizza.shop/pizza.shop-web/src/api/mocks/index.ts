80fa1856ceb30c895c904fc77e0cc028a79f067c
Raquel Santos <m-raquel@outlook.com.br>
2025-01-26 14:01:06 -0300

    call mock

--- pizza.shop/pizza.shop-web/src/api/mocks/index.ts
+++ pizza.shop/pizza.shop-web/src/api/mocks/index.ts
@@ -10,6 +10,10 @@ import { getDailyRevenueInPeriodMock } from './dashboard/get-daily-revenue-in-pe
 import { getPopularProductsMock } from './dashboard/get-popular-products-mock'
 import { getOrdersMock } from './orders/get-orders-mock'
 import { getOrderDetailsMock } from './orders/get-orders-details-mock'
+import { approveOrderMock } from './orders/status/approve-orders-mock'
+import { cancelOrderMock } from './orders/status/cancel-orders-mock'
+import { deliverOrderMock } from './orders/status/deliver-order-mock'
+import { dispatchOrderMock } from './orders/status/dispatch-order-mock'
 
 export const worker = setupWorker(
   signInMock,
@@ -22,6 +26,10 @@ export const worker = setupWorker(
   getPopularProductsMock,
   getOrdersMock,
   getOrderDetailsMock,
+  approveOrderMock,
+  cancelOrderMock,
+  deliverOrderMock,
+  dispatchOrderMock
 )
 
 export async function enableWorker() {
