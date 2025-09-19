1ce0493624a6453d872af95d4b7f0010a9744478
Raquel Santos <m-raquel@outlook.com.br>
2025-09-19 15:58:33 -0300

    test e2e: edit store profile

--- pizza.shop/pizza.shop-web/test/profile/store-profle.e2e.spec.ts
+++ pizza.shop/pizza.shop-web/test/profile/store-profle.e2e.spec.ts
@@ -2,7 +2,7 @@ import { expect, test } from '@playwright/test'
 
 test('update profile successfully', async ({ page }) => {
   await page.goto('/', { waitUntil: 'networkidle' })
-  await page.getByRole('button').filter({ hasText: /^$/ }).click()
+  await page.getByRole('button', { name: 'Pizza Shop' }).click()
   await page.getByRole('menuitem', { name: 'Perfil da loja' }).click()
 
   await page.locator('input[name="name"]').fill('Rocket pizza')
@@ -14,5 +14,8 @@ test('update profile successfully', async ({ page }) => {
   const toast = page.getByText('Perfil atualizado com sucesso!')
   expect(toast).toBeVisible()
 
-  // await page.waitForTimeout(2000)
+  await page.getByRole('button', { name: 'Close' }).click()
+  expect(page.getByRole('button', { name: 'Rocket pizza' })).toBeVisible()
+
+  await page.waitForTimeout(2000)
 })
