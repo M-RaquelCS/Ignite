0d0744a77ce4d523c1669d9bdd8b8d06356567e8
Raquel Santos <m-raquel@outlook.com.br>
2025-09-20 01:27:42 -0300

    dev: create a carousel with keen-slider

--- ignite_shop/ignite-shop-front/src/app/page.tsx
+++ ignite_shop/ignite-shop-front/src/app/page.tsx
@@ -1,35 +1,47 @@
+"use client";
+
 import Image from "next/image";
 import { HomeContainer, ProductContainer } from "@/styles/pages/home";
+import 'keen-slider/keen-slider.min.css'
+
+import { useKeenSlider } from "keen-slider/react";
+
 import tshirt1 from "../assets/tshirts/Shirt/1.png";
 import tshirt2 from "../assets/tshirts/Shirt/2.png";
 import tshirt3 from "../assets/tshirts/Shirt/3.png";
 import tshirt4 from "../assets/tshirts/Shirt/4.png";
 
 export default function Home() {
+  const [sliderRef] = useKeenSlider({
+    slides: {
+      perView: 3,
+      spacing: 48,
+    }
+  })
   return (
-    <HomeContainer>
-      <ProductContainer>
+    <HomeContainer ref={sliderRef} className="keen-slider">
+      <ProductContainer className="keen-slider__slide">
         <Image src={tshirt1} width={520} height={480} alt="T-Shirt 1" />
         <footer>
           <strong>T-Shirt 1</strong>
           <span>R$ 79,90</span>
         </footer>
       </ProductContainer>
-      <ProductContainer>
+      <ProductContainer className="keen-slider__slide">
         <Image src={tshirt2} width={520} height={480} alt="T-Shirt 2" />
         <footer>
           <strong>T-Shirt 2</strong>
           <span>R$ 79,90</span>
         </footer>
       </ProductContainer>
-      <ProductContainer>
+      <ProductContainer className="keen-slider__slide">
         <Image src={tshirt3} width={520} height={480} alt="T-Shirt 3" />
         <footer>
           <strong>T-Shirt 3</strong>
           <span>R$ 79,90</span>
         </footer>
       </ProductContainer>
-      <ProductContainer>
+      <ProductContainer className="keen-slider__slide">
         <Image src={tshirt4} width={520} height={480} alt="T-Shirt 4" />
         <footer>
           <strong>T-Shirt 4</strong>
