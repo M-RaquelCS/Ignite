72318de989921e677d2a84697e91953535a12eca
Raquel Santos <m-raquel@outlook.com.br>
2025-09-20 16:46:31 -0300

    add navigation link

--- ignite_shop/ignite-shop-front/src/app/(products)/index.tsx
+++ ignite_shop/ignite-shop-front/src/app/(products)/index.tsx
@@ -1,5 +1,6 @@
 import { useKeenSlider } from "keen-slider/react";
 import Image from "next/image";
+import Link from "next/link";
 import { HomeContainer, ProductContainer } from "@/styles/pages/home";
 
 interface ProductsProps {
@@ -22,14 +23,16 @@ export default function Products({ products }: ProductsProps) {
   return (
     <HomeContainer ref={sliderRef} className="keen-slider">
       {products?.map(product => (
-        <ProductContainer key={product.id} className="keen-slider__slide">
-          <Image src={product.imageUrl} width={520} height={480} alt="" />
+        <Link href={`/products/${product.id}`} key={product.id} prefetch={false}>
+          <ProductContainer className="keen-slider__slide">
+            <Image src={product.imageUrl} width={520} height={480} alt="" />
 
-          <footer>
-            <strong>{product.name}</strong>
-            <span>{product.price}</span>
-          </footer>
-        </ProductContainer>
+            <footer>
+              <strong>{product.name}</strong>
+              <span>{product.price}</span>
+            </footer>
+          </ProductContainer>
+        </Link>
       ))}
     </HomeContainer>
   )
